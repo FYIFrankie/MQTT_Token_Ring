c7084a49dc78c742ffec6dcc7e9518cda7e1974e
FrankieG <Frankie@Uoregon.edu>
2016-04-05 16:59:54 -0700

    Update

--- main.py
+++ main.py
@@ -87,6 +87,7 @@ def on_message(client, userdata, msg):
 		client.subscribe(u_neighbor)
 		print("Subscribed to " + u_neighbor)
 		client.will_set(get_lan_ip(), "dead - " + u_neighbor)
+		client.disconnect()
 		client.reconnect()
 	else:
 		print(msg.topic+" "+str(msg.payload))
