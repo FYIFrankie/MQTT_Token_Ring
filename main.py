6e934874a49c5e32d9e29db9fcf4e0c29e0012ce
FrankieG <Frankie@Uoregon.edu>
2016-04-07 19:23:27 -0700

    Update

--- main.py
+++ main.py
@@ -110,12 +110,14 @@ def on_message(client, userdata, msg):
 		client.subscribe(u_neighbor)
 		print("Subscribed to " + u_neighbor)
 	elif "election - " in msg.payload:
-		print("Election")
 		msg_UID = msg.payload[11:]
+		print("msg_UID is" + msg_UID)
+		print("my UID is " + UID)
 		if int(msg_UID) > UID:
 			print("Greater")
 			participant = True
 			client.publish(IP, msg.payload)
+
 		elif msg_UID < UID and participant == False:
 			print("Less than")
 			participant = True
